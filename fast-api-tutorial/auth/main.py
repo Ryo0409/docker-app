8287a0b877a007a6098524774db6b63d1d5c326d
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-05-07 21:53:52 +0900

    Create main.py

---
+++ fast-api-tutorial/auth/main.py
@@ -0,0 +1,41 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+
+import models, crud, schemas
+from database import SessionLocal, engine
+from auth import create_access_token, authenticate_user, get_current_active_user
+from dependency import get_db
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/users/me", response_model=schemas.User)
+async def read_users_me(current_user: models.User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.post("/users", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="username already registered")
+    return crud.create_user(db=db, user=user)
+
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
