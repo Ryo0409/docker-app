e8ad0ef9e918e1331b44d493df1c5100e83f74a2
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-03 00:25:19 +0900

    Update schemas.py

--- fastapi-auth-db/schemas.py
+++ fastapi-auth-db/schemas.py
@@ -1,34 +1,6 @@
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
-    email: str
-    username: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    hashed_password: str
-    items: [Item] = []
-
-    class Config:
-        orm_mode = True
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
 class ItemBase(BaseModel):
     name: str
 
@@ -43,3 +15,31 @@ class Item(ItemBase):
 
     class Config:
         orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    hashed_password: str
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
