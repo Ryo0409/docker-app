cd03c9c34831d011efb2d56f2a5717af8d44fcf6
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-05 22:39:42 +0900

    Create crud.py

---
+++ fastapi-auth-db/common/crud.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+
+from common import models, schemas
+from common.security import get_password_hash
+
+
+def get_user(db: Session, user_id: str):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_users(db: Session, skip: get_user = 0, limit: get_user = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(email=user.email, username=user.username, hashed_password=get_password_hash(user.password))
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, user_id: int):
+    return db.query(models.Item).filter(models.Item.owner_id == user_id).all()
+
+
+def create_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
