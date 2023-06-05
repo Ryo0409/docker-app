ff666061b1c709c3de7078c212d01ed38a90c8db
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-06 00:39:49 +0900

    Update auth_api.py

--- fastapi-auth-db/api/auth_api.py
+++ fastapi-auth-db/api/auth_api.py
@@ -17,7 +17,7 @@ router = APIRouter()
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
