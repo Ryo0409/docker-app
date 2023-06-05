8d5fe61d2dc012707ef439fceeb4c522ec19d41c
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-05 22:30:42 +0900

    Update main.py

--- fastapi-auth-db/main.py
+++ fastapi-auth-db/main.py
@@ -1,55 +1,12 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm import Session
-from datetime import datetime, timedelta
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI
 
-import models, crud, schemas
-from database import SessionLocal, engine
-from auth import create_access_token, authenticate_user, get_current_active_user
-from dependency import get_db
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from api import db_api, auth_api
+from common import models
+from common.session import engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/users/me", response_model=schemas.User)
-async def read_users_me(current_user: models.User = Depends(get_current_active_user)):
-    return current_user
-
-
-@app.post("/users", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="username already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.post("/token", response_model=schemas.Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-
-
-@app.post("/users/me/items/", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    current_user = read_users_me()
-    return crud.create_user_item(db=db, item=item, user_id=current_user.id)
-
-
-@app.get("/users/me/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    current_user = read_users_me()
-    items = crud.get_items(db, skip=skip, limit=limit, user_id=current_user.id)
-    return items
+app.include_router(db_api.router)
+app.include_router(auth_api.router)
