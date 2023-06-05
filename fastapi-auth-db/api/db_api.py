45c233c38cb889e855171bc471a64bc99e513672
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-06 00:39:27 +0900

    Update db_api.py

--- fastapi-auth-db/api/db_api.py
+++ fastapi-auth-db/api/db_api.py
@@ -10,12 +10,12 @@ router = APIRouter()
 
 
 @router.get("/users/me", response_model=schemas.User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
+def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
 
 @router.post("/users", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="username already registered")
@@ -23,11 +23,11 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/users/me/items", response_model=schemas.Item)
-async def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     return crud.create_item(db=db, item=item, user_id=current_user.id)
 
 
 @router.get("/users/me/items", response_model=list[schemas.Item])
-async def read_items(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+def read_items(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     items = crud.get_items(db, user_id=int(current_user.id))
     return items
