4491f18fb840c4abde4803c9878eca543bf5dc3c
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-05 23:42:57 +0900

    Update db_api.py

--- fastapi-auth-db/api/db_api.py
+++ fastapi-auth-db/api/db_api.py
@@ -15,11 +15,12 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 
 
 @router.post("/users", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="username already registered")
     return crud.create_user(db=db, user=user)
+
 
 @router.post("/users/me/items", response_model=schemas.Item)
 async def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
