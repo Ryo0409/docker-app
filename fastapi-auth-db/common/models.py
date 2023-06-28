18441331aff6bdb099ebc0327ac2233f61c1271b
Ryo0409 <44762962+Ryo0409@users.noreply.github.com>
2023-06-28 20:49:42 +0900

    Update models.py

--- fastapi-auth-db/common/models.py
+++ fastapi-auth-db/common/models.py
@@ -8,9 +8,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    email = Column(String(300), unique=True, index=True)
-    hashed_password = Column(String(300))
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
     notes = relationship("Note", back_populates="owner")
@@ -19,8 +19,8 @@ class Note(Base):
     __tablename__ = "notes"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50), index=True)
-    description = Column(String(300), index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="notes")
